5cb6e33a5a152dd0b367e96ea3afd43c92b52c45
Owawl <https://github.com/jiyk528/Test>
2022-05-01 16:16:36 +0900

    Sign up function

--- whisky/common/urls.py
+++ whisky/common/urls.py
@@ -7,4 +7,5 @@ app_name = 'common'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),  #로그인뷰 따로 만들필요 없음 대박
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
